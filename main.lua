d434273b1f736863c044484ae55cac6335673d4f
siddharth <siddhumyname@gmail.com>
2025-03-22 11:48:14 +0530

    tileset and camera implemented

--- main.lua
+++ main.lua
@@ -1,21 +1,30 @@
 function love.load()
+
+    camera = require('libraries/camera')
+    cam = camera()
     --to import anim8 lib
-    anim8 = require('anim8')
+    anim8 = require('libraries/anim8')
 
     --to make the filter work properly
     love.graphics.setDefaultFilter("nearest", "nearest")
 
+    sti = require('libraries/sti')-- to import tileset support
+    gamemap = sti('sprites/tileset/tileset.lua')
+
     --Player Basic Attributes
     player = {}
-    player.x = 10
-    player.y = 10
-    player.speed = 1
+    player.x = 300
+    player.y = 280
+    player.speed = 2
     player.facing = "right"
     player.cardSpriteSheet = love.graphics.newImage('sprites/Cards.png')
     player.cardGrid = anim8.newGrid(100, 144, player.cardSpriteSheet:getWidth(), player.cardSpriteSheet:getHeight())
 
-    player.spriteSheetRun = love.graphics.newImage('sprites/_Run.png') --assigning the spritesheet
-    player.gridRun = anim8.newGrid(48, 48, player.spriteSheetRun:getWidth(), player.spriteSheetRun:getHeight()) --assigning the grid using the given spritesheet (size of the sprite and size of the canvas)
+    player.spriteSheetRightRun = love.graphics.newImage('sprites/_RunRight.png') --assigning the spritesheet
+    player.gridRunRight = anim8.newGrid(48, 48, player.spriteSheetRightRun:getWidth(), player.spriteSheetRightRun:getHeight()) --assigning the grid using the given spritesheet (size of the sprite and size of the canvas)
+
+    player.spriteSheetRunLeft = love.graphics.newImage('sprites/_RunLeft.png')
+    player.gridRunLeft = anim8.newGrid(48, 48, player.spriteSheetRunLeft:getWidth(), player.spriteSheetRunLeft:getHeight())
     
     player.spriteSheetIdle = love.graphics.newImage('sprites/_Idle.png')
     player.gridIdle = anim8.newGrid(48, 48, player.spriteSheetIdle:getWidth(), player.spriteSheetIdle:getHeight())
@@ -23,7 +32,8 @@ function love.load()
     player.animation = {}
 
     player.cardAnim = anim8.newAnimation(player.cardGrid('1-15', 4), 2)
-    player.animation.run = anim8.newAnimation(player.gridRun('1-10', 1), 0.075) --setting up animation using the grid and the frames inside the grid with starting index and speed of next frame transition
+    player.animation.runRight = anim8.newAnimation(player.gridRunRight('1-10', 1), 0.075) --setting up animation using the grid and the frames inside the grid with starting index and speed of next frame transition
+    player.animation.runLeft = anim8.newAnimation(player.gridRunLeft('1-10', 1), 0.075)
     player.animation.idle = anim8.newAnimation(player.gridIdle('1-10', 1), 0.25)
 
 
@@ -37,15 +47,15 @@ function love.update(dt)
     if love.keyboard.isDown("d") then
         player.x = player.x + player.speed
         player.facing = "right"
-        player.spriteSheet = player.spriteSheetRun
-        player.anim = player.animation.run
+        player.spriteSheet = player.spriteSheetRightRun
+        player.anim = player.animation.runRight
         isMoving = true
     end
     if love.keyboard.isDown("a") then
         player.x = player.x - player.speed
         player.facing = "left"
-        player.spriteSheet = player.spriteSheetRun
-        player.anim = player.animation.run
+        player.spriteSheet = player.spriteSheetRunLeft
+        player.anim = player.animation.runLeft
         isMoving = true
     end
 
@@ -54,10 +64,14 @@ function love.update(dt)
         player.anim = player.animation.idle
     end
     player.anim:update(dt)
+    cam:lookAt(player.x, player.y)
 end
 
 function love.draw()
-    local scaleX = (player.facing == "left") and -4 or 4
-    player.anim:draw(player.spriteSheet, player.x, player.y, nil, scaleX, 4, 32, 0)
-    --player.anim:draw(player.cardSpriteSheet, player.x, player.y)
+    cam:attach()
+        gamemap:drawLayer(gamemap.layers['Platform'])
+        gamemap:drawLayer(gamemap.layers['Trees'])
+        gamemap:drawLayer(gamemap.layers['Grass'])
+        player.anim:draw(player.spriteSheet, player.x, player.y, nil, 2, nil, 24, 24)
+    cam:detach()
 end
